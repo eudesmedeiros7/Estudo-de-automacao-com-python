ab73cbdec8420065086ca2ebd58b630f3b44ab9e
Eudes Medeiros <eudes.medeiros7@hotmail.com>
2024-04-10 14:38:13 -0300

    troquei nome das pastas

---
+++ Aula_3/app.py
@@ -0,0 +1,10 @@
+# entrar na planilha
+# copiar informações de um campo e colar no seu campo
+# repetir esses passos para outros campos até preencher campos daquela pagina
+# clicar em próxima
+# repetir os mesmos passos e ir para a proxima pagina (pag. 2)
+# repetir o msm passo e finalizar o cadastro daquele produto e clicar em concluir
+# clicar em ok para finalizar o processo
+# clicar no ok mais uma vez na mensagem para confirmação de salvamento no banco de dados
+# clicar em adicionar mais 1
+
